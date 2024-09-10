a726e45da11649a319fdf1437980d9c0c7634055
Atif Pathan <72819433+Atif-Pathan@users.noreply.github.com>
2024-09-10 13:44:21 -0600

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const pali = string.toLowerCase().replace(/[! "#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').split('');
+    left = 0;
+    right = pali.length - 1;
+    while (left <= right) {
+        if (pali[left] !== pali[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
 };
 
 // Do not edit below this line
