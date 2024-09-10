57df3c96abee11399ede92439646094d6824088a
Atif Pathan <72819433+Atif-Pathan@users.noreply.github.com>
2024-09-10 12:09:59 -0600

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let rev = "";
+    for (let index = string.length - 1; index >= 0; index--) {
+        rev += string[index];
+    }
+    return rev;
 };
 
 // Do not edit below this line
