df3fcd332757ddb4bd30a25bd4f67aec8bbeb8ff
Atif Pathan <72819433+Atif-Pathan@users.noreply.github.com>
2024-09-10 13:05:52 -0600

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR";
+    }
+    let sumAll = 0;
+    end = Math.max(a,b);
+    start = Math.min(a,b);
+    for (let index = start; index < end + 1; index++) {
+        sumAll += index;  
+    }
+    return sumAll;
 };
 
 // Do not edit below this line
