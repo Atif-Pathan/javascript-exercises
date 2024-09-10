41a32594db4b3dbcd3b9b7bb9c595d532671edd6
Atif Pathan <72819433+Atif-Pathan@users.noreply.github.com>
2024-09-10 13:32:18 -0600

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a , b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  array.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let prod = 1;
+  array.forEach(element => {
+    prod *= element;
+  });
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let fact = 1;
+  for (let index = 1; index < x + 1; index++) {
+    fact *= index;
+  }
+  return fact;
 };
 
 // Do not edit below this line
