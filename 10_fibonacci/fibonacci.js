8b630968e04f2c1e4798d809afe1cacf2d65ecab
Atif Pathan <72819433+Atif-Pathan@users.noreply.github.com>
2024-09-10 14:00:09 -0600

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (!Number.isInteger(number)) {
+        number = parseInt(number);
+    }
+
+    if (number < 0) {
+        return "OOPS";
+    }
+    else if (number > 0 && number < 3) {
+        return 1;
+    }
+    else if (number === 0) {
+        return 0;
+    }
+
+    let f0 = 1;
+    let f1 = 1;
+    let fCurrent;
+    for (let index = 1; index < number - 1; index++) {
+        fCurrent = f0 + f1;
+        f0 = f1;
+        f1 = fCurrent;
+    }
+    return fCurrent;
 
 };
 
