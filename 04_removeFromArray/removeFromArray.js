23661285af3c37291a4526983f5dfad377e1edb1
Atif Pathan <72819433+Atif-Pathan@users.noreply.github.com>
2024-09-10 12:54:33 -0600

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const argArray = [...args];
+    const removedArray = array.filter(removeAllFromArray);
+
+    function removeAllFromArray(arg) {
+        if (!argArray.includes(arg)){
+            return arg;
+        }
+    }
+    return removedArray;
+
 };
 
 // Do not edit below this line
