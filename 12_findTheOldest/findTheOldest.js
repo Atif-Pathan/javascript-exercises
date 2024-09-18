5bcb65e10b0f8db4fd9526add55d3abfafe32718
Atif Pathan <72819433+Atif-Pathan@users.noreply.github.com>
2024-09-17 18:08:07 -0600

    Finish exercise 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(people) {
+    const oldestPerson = people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return oldestAge < currentAge ? current : oldest;
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
